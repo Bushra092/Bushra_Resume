25bd4791821f2bf5979772028ec8420632840f5c
Bushra Khan <96587982+Bushra092@users.noreply.github.com>
2024-04-03 17:18:21 +0530

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -20,3 +20,11 @@ abtMe.addEventListener("click", (e) => {
     }
   }
 });
+
+let menu = document.getElementById("sideMenu");
+function closeMenu() {
+  menu.style.right = "-250px";
+}
+function openMenu() {
+  menu.style.right = "0";
+}
