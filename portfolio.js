b7464c4d91c0b7164a148ce598a49e2e609ebaa0
Bushra Khan <96587982+Bushra092@users.noreply.github.com>
2024-05-03 01:41:07 +0530

    resume updated

--- portfolio.js
+++ portfolio.js
@@ -1,26 +1,3 @@
-const abtMe = document.querySelector(".tabtitles");
-const tabContains = document.querySelectorAll(".tabContains");
-
-abtMe.addEventListener("click", (e) => {
-  let targetElem = e.target;
-  let cssClass = targetElem.classList.contains("activeLink");
-
-  let idElem = targetElem.innerText.toLowerCase();
-  if (e.target.classList.contains("tablinks")) {
-    if (!cssClass) {
-      let oldActiveTab = document.querySelector(".activeLink");
-      oldActiveTab.classList.remove("activeLink");
-      targetElem.classList.add("activeLink");
-    }
-
-    for (let i = 0; i < tabContains.length; i++) {
-      tabContains[i].id === idElem
-        ? (tabContains[i].style.display = "block")
-        : (tabContains[i].style.display = "none");
-    }
-  }
-});
-
 let menu = document.getElementById("sideMenu");
 function closeMenu() {
   menu.style.right = "-250px";
